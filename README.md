447a84e95359054a614ef2ba1873392c5e669c84
yo <potpot-masta@hotmail.fr>
2023-03-02 14:15:23 +0100

    - finish app

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # offer-api
+
+## Describe :
+
+This a Offer Api, how do use it.
+
+## Required :
+
+ - Java 17 Oracle Or OpenJDK, (i use OpenJDK)
+ - Maven 3.9.0
+
+## Run
+
+ - mvn clean package spring-boot:run
+
+## Swagger
+
+- [Swagger Offer Api](http://localhost:9000/swagger-ui/index.html)
+
+## Postman collection
+
+- Into project resources directory
--- src/main/java/com/technical/offer/api/ApiApplication.java
+++ src/main/java/com/technical/offer/api/ApiApplication.java
@@ -1,8 +1,11 @@
 package com.technical.offer.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Offer API", version = "1.0", description = " Offer Api for User"))
 @SpringBootApplication
 public class ApiApplication {
 
